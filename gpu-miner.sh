6e04419b9228667aabb9fac7db71a73a484063f3
hbesthee <hbesthee@naver.com>
2020-08-16 15:07:18 +0900

    convert space to tab

--- gpu-miner.sh
+++ gpu-miner.sh
@@ -8,10 +8,10 @@ TARGET_MINER=t-rex
 miner=`ps -ef | grep "${TARGET_MINER}" | grep -v 'grep' | grep -v 'sh -c' | awk '{print $2}'`
 if [ -z $miner ]; then
 
-   echo $(date)
+	echo $(date)
 
-   screen -dmS gpu bash -c "/home/miner/bin/${TARGET_COIN}-start.sh"
+	screen -dmS gpu bash -c "/home/miner/bin/${TARGET_COIN}-start.sh"
 
-   echo "" 
+	echo "" 
 
 fi
